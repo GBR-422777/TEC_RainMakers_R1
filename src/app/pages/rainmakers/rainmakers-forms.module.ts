6ed91b9da07f1c21aab708966c550e9c77344a65
Mohammad Omar <mohammad0omar@outlook.com>
2019-09-10 13:38:14 +0300

    mod: "modify rainmakers form module and add new components and declare them"

--- src/app/pages/rainmakers/rainmakers-forms.module.ts
+++ src/app/pages/rainmakers/rainmakers-forms.module.ts
@@ -12,6 +12,10 @@ import {
 } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
+
+import { Test2Component } from './form/test2/test2.component';
+import { Test1Component } from './form/test1/test1.component';
+import { RainmakersFormsRoutingModule } from './rainmakers-form-routing.module';
 import { RainmakersFormComponent } from './rainmakers-form-inputs.component';
 
 @NgModule({
@@ -27,9 +31,11 @@ import { RainmakersFormComponent } from './rainmakers-form-inputs.component';
     NbDatepickerModule,
     NbSelectModule,
     NbIconModule,
+    RainmakersFormsRoutingModule,
+  
   ],
   declarations: [
-    RainmakersFormComponent,
+    Test1Component,Test2Component,RainmakersFormComponent
   ],
 })
 export class RainmakersFormsModule { }
