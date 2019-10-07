ac9ba56d01ed5825c85395746d289ba50c7d7e33
ahmadAtrach <54948072+ahmadAtrach@users.noreply.github.com>
2019-10-07 14:49:56 +0300

    add: ability to post new rainmakers from api and save them to mongo

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 const db_name = 'tec';
-
 mongoose.connect('mongodb://localhost:27017/' + db_name, {useNewUrlParser: true});
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 const Rainmaker = mongoose.model('Rainmaker', { name: String, major: String, age: Number });
 
-app.get('/', (req, res) => 
+app.post('/', (req, res) => 
 {
-    const rainmaker = new Rainmaker({ name: "georges", major: "computer science", age: 20 });
-    rainmaker.save().then(() => res.send('Hello World! georges is saved'));
+    console.log(req.body);
+    const rainmaker = new Rainmaker({ name: req.body.name, major: req.body.major, age: req.body.age });
+    rainmaker.save().then(() => res.send("Hello a new Rainmaker was added: " + req.body.name));
 })
 
 app.listen(port, () => console.log(`The app listening on port ${port}!`))
